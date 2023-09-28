aa25ec1e31898396032792177179c9a3859f8e1f
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-09-28 14:17:41 +0200

    Create match.py

---
+++ match.py
@@ -0,0 +1,30 @@
+class Match(object):
+    def __init__(self, team_1: int, team_2: int):
+        self.team_1 = team_1
+        self.team_2 = team_2
+
+    def __repr__(self) -> str:
+        return f'Match({self.team_1}, {self.team_2})'
+
+    def __str__(self) -> str:
+        return f'{self.team_1} vs {self.team_2}'
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(self, match: object):
+        return self.team_1 == match.team_1 and self.team_2 == match.team_2
+
+    def __contains__(self, team):
+        return self.team_1 == team or self.team_2 == team
+
+    def to_tuple(self):
+        return self.team_1, self.team_2
+
+    # Check if two matches have at least a team in common and so are in conflict
+    def conflict(self, match: object) -> bool:
+        if self.team_1 == match.team_1 or self.team_1 == match.team_2 \
+                or self.team_2 == match.team_1 or self.team_2 == match.team_2:
+            return True
+        else:
+            return False
